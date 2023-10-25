beb73304c248ebfc94979c8e27f52dace356ebde
rayhankobir-dev <rayhankobir793@gmail.com>
2023-10-26 01:41:45 +0600

    initialize with basic functionality

---
+++ src/utils/Themes.js
@@ -0,0 +1,19 @@
+export const darkTheme = {
+  bg: "#15171E",
+  bgLight: "#1C1E27",
+  primary: "#00cc66",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#121212",
+  button: "#00cc66",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#00cc66",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#00cc66",
+};
